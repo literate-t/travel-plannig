713a3582acb4049bcd333c868f56369562677419
literate-t <gaekoon@gmail.com>
2025-12-30 17:13:05 +0900

    feat: api 요청은 vite를 통해 프록시한다

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   root: ".",
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
 });
