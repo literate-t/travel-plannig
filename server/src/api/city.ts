2da16c7f102e06e589a727fd687a681c2b9908e8
literate-t <gaekoon@gmail.com>
2025-12-30 17:11:41 +0900

    feat: 기본적인 city router를 설정한다.

---
+++ server/src/api/city.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import { City } from "../entity/city.entity.js";
+import { CityRepository } from "../repository/city.repository.js";
+import { CityDto } from "../types.js";
+
+const cityRouter = Router();
+
+cityRouter.get("/", async (req, res) => {
+  try {
+    const result = await CityRepository.findAll();
+    res.send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+cityRouter.get("/:city", async (req, res) => {
+  try {
+    const result = await CityRepository.findByName(req.params.city);
+    res.send(result);
+  } catch (err) {
+    res.status(500).send("Can't find data");
+  }
+});
+
+cityRouter.post("/", async (req, res) => {
+  const city = req.body as CityDto;
+
+  try {
+    const result = await CityRepository.save(City.ofDto(city));
+    res.send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+export default cityRouter;
