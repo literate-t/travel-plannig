6096e31a49c4e65770b4c08fa6071fc9e1a2917b
literate-t <gaekoon@gmail.com>
2026-01-02 00:58:34 +0900

    feat: react-router-dom을 적용한다

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
-import RegisterCity from "@/components/admin/RegisterCity";
+import Loading from "@/components/common/Loading";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+const RegisterCity = lazy(() => import("@/pages/admin/RegisterCity"));
 
 function App() {
-  return <RegisterCity />;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/admin">
+            <Route path="register-city" element={<RegisterCity />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
