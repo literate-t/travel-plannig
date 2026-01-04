a65e6510a9a76916a70d8b3571a1e0eeb59c4bcf
literate-t <gaekoon@gmail.com>
2026-01-04 12:10:08 +0900

    feat: 새로 추가한 엔티티를 DataSource가 읽을 수 있도록 한다 - 개발 환경인 tsx 환경에서는 js 데이터를 메모리에 저장할 수 있는데 typeorm은 물리적으로 해당 파일을 찾기 때문에, 엔티티 정보를 못 찾을 수 있다 - 일부 타입스크립트 실행 도구는 실제 디스크 상의 파일 구조와 런타임 상의 파일 구조를 다르게 가져가는 경우가 있으므로 직접 객체 정보를 주입하는 방법으로 진행한다.

--- server/src/entity/index.ts
+++ server/src/entity/index.ts
@@ -1,1 +1,4 @@
 export { City } from "./city.entity.js";
+export { CityCoordiate } from "./coordinate.entity.js";
+export { Country } from "./country.entity.js";
+export { Visa } from "./visa.entity.js";
