88f40ab59c18d6751e72913f311f8a6db5ac5981
literate-t <gaekoon@gmail.com>
2026-01-04 12:01:41 +0900

    feat: 도시 엔티티의 변경을 반영한다 - 컬럼 정보가 더 자세하게 추가된다 - 좌표, 국가 정보는 별도의 엔티티를 통해 연관관계를 맺는다

--- server/src/entity/city.entity.ts
+++ server/src/entity/city.entity.ts
@@ -1,5 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { CityDto } from "../types.js";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { CityDto, CoordinateDto, CountryDto } from "../types.js";
+import { CityCoordiate } from "./coordinate.entity.js";
+import { Country } from "./country.entity.js";
+import { Visa } from "./visa.entity.js";
 
 @Entity()
 export class City {
@@ -7,23 +16,92 @@ export class City {
   id!: number;
 
   @Column({ type: "varchar" })
+  code!: string;
+
+  @Column({ type: "varchar" })
   name!: string;
 
   @Column({ type: "varchar" })
-  city!: string;
+  nameEn!: string;
+
+  @Column({ type: "varchar" })
+  thumbnail!: string;
 
   @Column({ type: "text" })
   description!: string;
 
-  static of(name: string, city: string, description: string): City {
+  @Column({ type: "varchar" })
+  timezone!: string;
+
+  @Column({ type: "int8" })
+  flighthour!: number;
+
+  @Column({ type: "int8" })
+  timezoneOffset!: number;
+
+  @OneToOne(() => CityCoordiate)
+  @JoinColumn()
+  coordinate!: CityCoordiate;
+
+  @OneToOne(() => Country)
+  @JoinColumn()
+  country!: Country;
+
+  static of(
+    code: string,
+    name: string,
+    nameEn: string,
+    thumbnail: string,
+    description: string,
+    timezone: string,
+    flighthour: number,
+    timezoneOffset: number,
+    coordinate: CoordinateDto,
+    country: CountryDto
+  ): City {
     const newCity = new City();
+    newCity.code = code;
     newCity.name = name;
-    newCity.city = city;
+    newCity.nameEn = nameEn;
+    newCity.thumbnail = thumbnail;
     newCity.description = description;
+    newCity.timezone = timezone;
+    newCity.flighthour = flighthour;
+    newCity.timezoneOffset = timezoneOffset;
+
+    const newCoord = new CityCoordiate();
+    newCoord.lat = coordinate.lat;
+    newCoord.lng = coordinate.lng;
+    newCity.coordinate = newCoord;
+
+    const newCountry = new Country();
+    newCountry.code = country.code;
+    newCountry.name = country.name;
+    newCountry.nameEn = country.nameEn;
+    newCountry.voltage = country.voltage;
+
+    const newVisa = new Visa();
+    newVisa.duration = country.visa.duration;
+    newVisa.required = country.visa.required;
+    newCountry.visa = newVisa;
+    newCountry.continent = country.continent;
+    newCity.country = newCountry;
+
     return newCity;
   }
 
   static ofDto(dto: CityDto): City {
-    return City.of(dto.name, dto.city, dto.description);
+    return this.of(
+      dto.code,
+      dto.name,
+      dto.nameEn,
+      dto.thumbnail,
+      dto.description,
+      dto.timezone,
+      dto.flighthour,
+      dto.timezoneOffset,
+      dto.coordinate,
+      dto.country
+    );
   }
 }
