bad00d9eba18271aebf73f4249ff639085632ee8
literate-t <gaekoon@gmail.com>
2026-01-04 12:00:27 +0900

    feat: 도시 데이터를 불러올 때 1:1 연관관계에 있는 국가 데이터를 같이 불러온다

--- server/src/api/city.ts
+++ server/src/api/city.ts
@@ -5,10 +5,17 @@ import { CityDto } from "../types.js";
 
 const cityRouter = Router();
 
-cityRouter.get("/", async (req, res) => {
+cityRouter.get("/", async (_req, res) => {
   try {
-    const result = await CityRepository.findAll();
-    res.send(result);
+    const cities = await CityRepository.find({
+      relations: ["country"],
+    });
+    if (!cities || cities.length === 0) {
+      res.status(404).send("City not found");
+    } else {
+      const result = cities.map((city) => ({ ...city }));
+      res.send(result);
+    }
   } catch (err) {
     res.status(500).send(err);
   }
@@ -16,9 +23,19 @@ cityRouter.get("/", async (req, res) => {
 
 cityRouter.get("/:city", async (req, res) => {
   try {
-    const result = await CityRepository.findByName(req.params.city);
-    res.send(result);
+    const city = await CityRepository.findOne({
+      where: {
+        code: req.params.city,
+      },
+      relations: ["country"],
+    });
+    if (!city) {
+      res.status(404).send("City not found");
+    } else {
+      res.send({ ...city });
+    }
   } catch (err) {
+    console.log("Error", err);
     res.status(500).send("Can't find data");
   }
 });
