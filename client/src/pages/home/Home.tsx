5391f98696addc75017bb4739508ce72937f5963
literate-t <gaekoon@gmail.com>
2026-01-05 22:25:38 +0900

    feat: openModal()로 모달 컴포넌트를 새로 생성 onClose()를 props로 받아 닫기 버튼에 바인딩한다
    - 실제로 넘기는 곳은 ModalProvider에서 확인할 수 있다

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import Loading from "@/components/common/Loading";
+import Modal, { ModalBackdrop, ModalPanel } from "@/components/common/Modal";
 import NarrowLayout from "@/components/common/NarrowLayout";
 import CityList from "@/components/home/CityList";
 import FilterList from "@/components/home/FilterList";
 import SearchInput from "@/components/home/SearchInput";
 import { getCities, getSearchedCities } from "@/services/home";
+import { useModalStore } from "@/store";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -13,18 +15,37 @@ export default function Home() {
     queryKey: ["cities", q],
     queryFn: q ? () => getSearchedCities(q) : getCities,
   });
+
+  const { openModal } = useModalStore();
+
+  const handleClick = () => {
+    openModal(({ onClose }) => (
+      <Modal>
+        <ModalBackdrop />
+        <ModalPanel>
+          <div className="bg-white">
+            <button onClick={onClose}>닫기</button>
+          </div>
+        </ModalPanel>
+      </Modal>
+    ));
+  };
+
   return isLoading || !data ? (
     <Loading />
   ) : (
-    <NarrowLayout className="flex flex-col items-center my-30">
-      <div className="w-[339px] mb-24">
-        {/* TODO: Debouncing */}
-        <SearchInput onCompositionEnd={(value) => setQ(value)} />
-      </div>
-      <div className="mb-21">
-        <FilterList active="all" onChange={() => {}} />
-      </div>
-      <CityList cities={data} />
-    </NarrowLayout>
+    <>
+      <button onClick={handleClick}>open modal</button>
+      <NarrowLayout className="flex flex-col items-center my-30">
+        <div className="w-[339px] mb-24">
+          {/* TODO: Debouncing */}
+          <SearchInput onCompositionEnd={(value) => setQ(value)} />
+        </div>
+        <div className="mb-21">
+          <FilterList active="all" onChange={() => {}} />
+        </div>
+        <CityList cities={data} />
+      </NarrowLayout>
+    </>
   );
 }
