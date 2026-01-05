7a9b2dacc4601574242d07b8403db1d7f7184563
literate-t <gaekoon@gmail.com>
2026-01-05 18:50:07 +0900

    feat: flex 박스의 너비에 w-full을 적용하고 아이템이 왼쪽에서부터 정렬되도록 한다

--- client/src/components/home/CityList.tsx
+++ client/src/components/home/CityList.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function CityList({ cities }: Props) {
   return (
-    <div className="flex flex-wrap justify-between gap-y-28">
+    <div className="flex flex-wrap justify-between gap-y-28 items-start w-full">
       {cities.map((city) => (
         <Card
           key={city.code}
