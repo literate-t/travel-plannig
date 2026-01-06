605236d8b4878749b7010cb7cfbf2d6ae3ffffb0
literate-t <gaekoon@gmail.com>
2026-01-06 14:53:47 +0900

    fix: DB 연결 이슈를 해결 - 로컬에서 포트를 안전하게 쓰기 위해 15432로 변경 - DataSource 객체를 만들 때 포트 번호 환경 변수의 오타를 수정

--- server/database/datasource.ts
+++ server/database/datasource.ts
@@ -9,7 +9,7 @@ dotenvExpand.expand(dotenv.config());
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTRGES_PORT),
+  port: Number(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
