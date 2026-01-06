416073d34c65a8ab7ecec702d984081ba478ca23
literate-t <gaekoon@gmail.com>
2026-01-06 13:52:33 +0900

    fix: 모달 임시 코드를 제거한다

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,11 +1,9 @@
 import Loading from "@/components/common/Loading";
-import Modal, { ModalBackdrop, ModalPanel } from "@/components/common/Modal";
 import NarrowLayout from "@/components/common/NarrowLayout";
 import CityList from "@/components/home/CityList";
 import FilterList from "@/components/home/FilterList";
 import SearchInput from "@/components/home/SearchInput";
 import { getCities, getSearchedCities } from "@/services/home";
-import { useModalStore } from "@/store";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -16,26 +14,10 @@ export default function Home() {
     queryFn: q ? () => getSearchedCities(q) : getCities,
   });
 
-  const { openModal } = useModalStore();
-
-  const handleClick = () => {
-    openModal(({ onClose }) => (
-      <Modal>
-        <ModalBackdrop />
-        <ModalPanel>
-          <div className="bg-white">
-            <button onClick={onClose}>닫기</button>
-          </div>
-        </ModalPanel>
-      </Modal>
-    ));
-  };
-
   return isLoading || !data ? (
     <Loading />
   ) : (
     <>
-      <button onClick={handleClick}>open modal</button>
       <NarrowLayout className="flex flex-col items-center my-30">
         <div className="w-[339px] mb-24">
           {/* TODO: Debouncing */}
