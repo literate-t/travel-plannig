f4191adfb710f3d931ec0104a4ff627ada5755ad
literate-t <gaekoon@gmail.com>
2026-01-09 13:36:13 +0900

    feat: vite 환경에서 env 항목의 타입이 자동완성으로 뜨게 하기 위해 기존 인터페이스를 확장한다

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  VITE_GOOGLE_MAPS_API_KEY: string;
+}
+
+interface ImportMeta {
+  env: ImportMetaEnv;
+}
