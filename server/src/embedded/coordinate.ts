6ff6ba34466dadcdd194530f0314484ddacd9499
literate-t <gaekoon@gmail.com>
2026-01-09 13:47:10 +0900

    feat: 서버에서 소수점 데이터를 가져올 때 문자열이 아니라 숫자 그대로 가져올 수 있도록 한다

--- server/src/embedded/coordinate.ts
+++ server/src/embedded/coordinate.ts
@@ -1,9 +1,25 @@
 import { Column } from "typeorm";
 
 export class CityCoordiate {
-  @Column({ type: "decimal", precision: 10, scale: 7 })
+  @Column({
+    type: "decimal",
+    precision: 10,
+    scale: 7,
+    transformer: {
+      to: (value: number) => value,
+      from: (value: string) => parseFloat(value),
+    },
+  })
   lat!: number;
 
-  @Column({ type: "decimal", precision: 10, scale: 7 })
+  @Column({
+    type: "decimal",
+    precision: 10,
+    scale: 7,
+    transformer: {
+      to: (value: number) => value,
+      from: (value: string) => parseFloat(value),
+    },
+  })
   lng!: number;
 }
