e90905d5875d45d9d1260a98890f87679cfbeb58
literate-t <gaekoon@gmail.com>
2026-01-10 02:12:39 +0900

    feat: 도시의 장소 데이터를 조회하는 api를 추가한다 - getPlaces()

--- client/src/services/plan.ts
+++ client/src/services/plan.ts
@@ -1,5 +1,15 @@
-import { CityDto } from "@/types";
+import { City, Place } from "@/types";
 
-export const getCity = async (cityId: string): Promise<CityDto> => {
+export const getCity = async (cityId: string): Promise<City> => {
   return fetch(`/api/cities/${cityId}`).then((res) => res.json());
+};
+
+export const getPlaces = async (
+  city: string,
+  query: { category?: string; q?: string } = {}
+): Promise<Place[]> => {
+  const queryString = new URLSearchParams(query).toString();
+  return fetch(
+    `/api/cities/${city}/places${queryString ? `?${queryString}` : ""}`
+  ).then((res) => res.json());
 };
